8c7adc8619a35359436b6b0fc1f0a6c180ff6473
dmarc <dmarcr1997@gmail.com>
2020-06-27 12:18:13 -0500

    added more options in clap and maud html wrap

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
+#![feature(proc_macro_hygiene)]
+
 use clap::{clap_app,crate_version};
 use pulldown_cmark::{html::push_html, Event, Parser};
+use maud::html;
+fn wrap_html(s:&str, css:Option<&str>)-> String{
+    let res = html!{
+        (maud::DOCTYPE)
+        html{
+            head{
+                meta charset="utf-8";
+                @if let Some(s) = css {
+                    link rel="stylesheet" type="text/css" href=(s) {}
+                }
+            }
+            body{
+                (maud::PreEscaped(s))
+            }
+        }
+    };
+    res.into_string()
+}
 fn main() {
     let clap = clap_app!( clapp =>
                             (version:crate_version!()) 
                             (author:"Damon Rocha")
                             (about:"Renders markdown as you like")
-                            (@arg input: + required "Sets the input file")                       
+                            (@arg input: + required "Sets the input file")  
+                            (@arg wrap: -w "Wrap in html")
+                            (@arg event: -e "Print event")
+                            (@arg css: --css + takes_value "Link to css")                     
     )
     .get_matches();
     println!("Input = {:?}", clap.value_of("input"));
@@ -16,12 +39,19 @@ fn main() {
     let mut res = String::new();
 
     let ps = Parser::new(&infile);
-
     let ps : Vec<Event> = ps.into_iter().collect();
-    for p in &ps{
-        println!("{:?}", p);
+
+    if clap.is_present("event"){
+   
+        for p in &ps{
+            println!("{:?}", p);
+        }
     }
 
     push_html(&mut res, ps.into_iter());
+    
+    if clap.is_present("wrap"){
+        res = wrap_html(&res, clap.value_of("css"));
+    }
     println!("{}", res);
 }
