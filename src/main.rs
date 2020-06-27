5d9724b30336be8e6db64cbb9acacf23bdbbc4e5
dmarc <dmarcr1997@gmail.com>
2020-06-27 12:02:14 -0500

    added clap and pulldown-cmark crates

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use clap::{clap_app,crate_version};
+use pulldown_cmark::{html::push_html, Event, Parser};
+fn main() {
+    let clap = clap_app!( clapp =>
+                            (version:crate_version!()) 
+                            (author:"Damon Rocha")
+                            (about:"Renders markdown as you like")
+                            (@arg input: + required "Sets the input file")                       
+    )
+    .get_matches();
+    println!("Input = {:?}", clap.value_of("input"));
+    let infile = 
+    std::fs::read_to_string(clap.value_of("input").unwrap())
+    .expect("Could not read file");
+
+    let mut res = String::new();
+
+    let ps = Parser::new(&infile);
+
+    let ps : Vec<Event> = ps.into_iter().collect();
+    for p in &ps{
+        println!("{:?}", p);
+    }
+
+    push_html(&mut res, ps.into_iter());
+    println!("{}", res);
+}
